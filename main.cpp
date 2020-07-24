e2407b04155f1bdec116cf47fb1f688681a9dbe7
liapassaglia <liapassaglia@gmail.com>
2020-07-23 20:03:24 -0400

    Added to Map class.

--- main.cpp
+++ main.cpp
@@ -21,19 +21,6 @@ string printRandomString(int n)
 
     return res;
 }
-
-// ===================== Map  ================
-class Tree
-{
-public:
-};
-
-// ===================== Tree  ================
-class Map
-{
-public:
-};
-
 // ===================== InventorySystem Class ================
 class InventorySystem
 {
@@ -62,6 +49,8 @@ public:
             Item item;
             item.name = printRandomString(10);
             item.idNumber = i + 1;
+            //randomized idNumbers
+            item.idNumber = rand() % 1000;
             item.inStock = rand() % 1000;
 
             // add item to inventory list/arry
@@ -78,6 +67,89 @@ public:
             cout << endl;
         }
     }
+};
+
+// ===================== Map  ================
+//currently based on the the keys being unsorted (if we wanted to sort them either numerically or alphabetically it would require a few changes)
+class Map
+{
+private:
+    vector<int> keys;
+    vector<InventorySystem::Item> values;
+    int size;
+public:
+    Map(vector<InventorySystem::Item> &inventory){
+        //storing the inventory data in this map data structure
+        //a key and its value are at same location in their corresponding containers. 
+        size = inventory.size();
+        for(int i = 0; i < size; i++){
+            //the key is the idNumber (could easily be changed)
+            keys.push_back(inventory.at(i).idNumber);
+            //the value is the Item itself 
+            values.push_back(inventory.at(i));
+        }
+    }
+    void printMap(){
+        for(int i = 0; i < size; i++){
+            cout << "idNumber: " << keys.at(i) << endl;
+            cout << "Name: " << values.at(i).name << endl;
+            cout << "Amount: " << values.at(i).inStock << endl;
+        }
+    }
+    void add(InventorySystem::Item item){
+        //simply adds the key and value to the end of corresponding vectors
+        //time complexity is O(1)
+        keys.push_back(item.idNumber);
+        values.push_back(item);
+        size++;
+    }
+    void remove(int idNumber){
+        //time complexity is log(n)
+        int index = helpFind(idNumber,0,size);
+        //this replaces the item to be removed with the last item and then deletes the last item which all can be done in O(1)
+        if(index >= 0){
+            keys.at(index) = keys.at(size - 1);
+            values.at(index) = values.at(size - 1);
+            keys.pop_back();
+            values.pop_back();
+            size--;
+        }
+        /* this would just delete the item to be removes and shifts all the other ones which is done in O(n)
+        if(index >= 0){
+            keys.erase(keys.begin() + index);
+            values.erase(values.begin() + index);
+        }*/
+    }
+    int helpFind(int idNumber, int first, int last){
+        //binarySearch so time complexity is log(n)
+        if(first > last)
+            return -1;
+        int middle = (first + last) / 2;
+        if(keys.at(middle) == idNumber)
+            return middle;
+        if(keys.at(middle) < idNumber)
+            return helpFind(idNumber, middle+1, last);
+        else
+            return helpFind(idNumber, first, middle-1);
+    }
+    void find(int idNumber){
+        //time complexity is O(log(n))
+        int index = helpFind(idNumber,0,size);
+        //print statements could be removed if we want to implement them in main instead
+        if(index < 0)
+            cout << "Item not found" << endl;
+        else{
+            cout << "Name: " << values.at(index).name << endl;
+            cout << "idNumber: " << keys.at(index) << endl;
+            cout << "Number of items: " << values.at(index).inStock << endl;
+        }
+    }
+};
+
+// ===================== Tree  ================
+class Tree
+{
+public:
 };
 
 // ===================== Test Cases ===========================
@@ -150,9 +222,20 @@ void test4(){
 void run()
 {
     InventorySystem system1;
-    // system1.inventoryGenerator(100);
-    // system1.printInventoryVector();
-
+    system1.inventoryGenerator(100);
+    system1.printInventoryVector();
+    /*Some possible function calls for testing purpose if you guys want to see how it works
+    Map map(system1.inventoryVector);  
+    InventorySystem::Item item;
+    item.name = printRandomString(10);
+    item.idNumber = 101;
+    item.inStock = 50;
+    map.add(item);
+    map.remove(1);
+    map.printMap();
+    cout << endl;
+    map.find(1);
+    */
     /*
         test1();
         test2();
@@ -169,6 +252,5 @@ int main()
 {
     // Main is used to test whatever you're working on
     // Just make sure to clear main before pushing/commiting any changes
-
     return 0;
 };
